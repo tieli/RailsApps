634305482267175e9ca97e1a0ceb0b28da2118f3
Tiejun Li <tiejli@yahoo.com>
2016-07-16 17:19:13 -0700

    clean up app_template.rb.

--- app_template.rb
+++ app_template.rb
@@ -134,7 +134,6 @@ article_model = ["Article", {
   "hidden"       => "boolean"
 }
 ]
-generate "scaffold", get_gen_str(article_model)
 
 comment_model = ["Comment", {
   "commenter" => "string",
@@ -143,7 +142,21 @@ comment_model = ["Comment", {
 }
 ]
 
+tag_model = ["tag", {
+  "name" => "string"
+}
+]
+
+tagging_model = ["tagging", {
+  "tag" => "belongs_to",
+  "article" => "belongs_to"
+}
+]
+
+generate "scaffold", get_gen_str(article_model)
 generate "model", get_gen_str(comment_model)
+generate "model", get_gen_str(tag_model)
+generate "model", get_gen_str(tagging_model)
 
 generate "controller", "comments"
 
@@ -151,130 +164,36 @@ route "root to: 'articles\#index'"
 
 rake "db:migrate"
 
-# #
-# Take care of Database
-#
-article_model_file = 'app/models/article.rb'
-inject_into_file article_model_file, before: "end" do <<-'RUBY'
-  has_many :comments
-  RUBY
-end
-
-route_config_file = 'config/routes.rb'
-inject_into_file route_config_file, after: "resources :articles" do <<-'RUBY'
-  do
-    resources :comments
-  end
-  RUBY
-end
 
 # 
 # Take care of front page
 #
 remove_file "public/index.html"
 
-app_html_erb = 'app/views/layouts/application.html.erb'
-inject_into_file app_html_erb, after: "<body>\n" do <<-'RUBY'
-	<div id="container">
-	<% flash.each do |name, msg| %>
-	      <%= content_tag :div, msg, :id => "flash_#{name}" %>
-	<% end %>
-	RUBY
-end
-
-inject_into_file app_html_erb, after: "<%= yield %>\n" do <<-'RUBY'
-	</div>
-	RUBY
-end
-
-ar_show_html_erb = 'app/views/articles/show.html.erb'
-inject_into_file ar_show_html_erb, before: "<%= link_to 'Edit'," do <<-'RUBY'
-  <p>
-  <%= render @article.comments %>
-  </p>
-
-  <p>
-  <%= render "comments/form" %>
-  </p>
-
-  RUBY
-end
-
-#comment_controller_rb = 'app/controllers/comments_controller.rb'
-#
-#inject_into_file comment_controller_rb, before: "end" do <<-'RUBY'
-#
-#  def create
-#    @article = Article.find(params[:article_id])
-#    @comment = @article.comments.create(comment_params)
-#    @comment.save
-#    redirect_to @article
-#  end
-#
-#  def edit
-#    @article = Article.find(params[:article_id])
-#    @comment = Comment.find(params[:id])
-#  end
-#
-#  def update
-#    @comment = Comment.find(params[:id])
-#    @article = Article.find(params[:article_id])
-#    if @comment.update(comment_params)
-#      redirect_to @article
-#    end
-#  end
-#
-#  def destroy
-#    @comment.destroy
-#  end
-#
-#  private
-#  def comment_params
-#    params.require(:comment).permit(:content, :commenter)
-#  end
-#  RUBY
-#end
-#
-#scaffolds_css = 'app/assets/stylesheets/scaffolds.scss'
-#append_file scaffolds_css, <<-'RUBY'
-##container {
-#	 width: 80%;
-#	 margin: 0 auto;
-#	 background-color: #FFF;
-#	 padding: 20px 40px;
-#	 border: solid 1px black;
-#	 margin-top: 20px;
-#}
-#RUBY
-#
-#bg_color_white = "background-color: #fff;"
-#bg_color_blue  = "background-color: #3300CC;"
-#
-#gsub_file scaffolds_css, bg_color_white, bg_color_blue
-
-# 
-# Taking care of testing.
-#
-#remove_file "db/seeds.rb"
-
 generate "rspec:install"
 
 capify!
 
-app_link = "https://bitbucket.org/tiejli/railsapps/raw"
-app_id   = "c434b843903ffe6d6c048fd8c2859e3afbe4109b"
-repo = "#{app_link}/#{app_id}"
+repo = "https://raw.githubusercontent.com/tieli/RailsApps/master/"
 
 app_files = ['app/assets/stylesheets/application.scss', 
+             'app/assets/stylesheets/scaffolds.scss',
              'app/views/layouts/application.html.erb',
+             'app/controllers/articles_controller.rb',
              'app/controllers/comments_controller.rb',
+             'app/views/comments/_comment.html.erb',
+             'app/views/comments/edit.html.erb',
+             'app/views/comments/_form.html.erb',
              'app/views/articles/index.html.erb',
              'app/views/articles/show.html.erb',
+             'app/views/articles/_form.html.erb',
              'app/models/article.rb',
-             'config/route.rb',
+             'config/routes.rb',
              'db/seeds.rb']
 
 app_files.each do |from_file|
   copy_from_repo 'blogs', from_file, :repo => repo
 end
+
+rake "db:seed"
 
