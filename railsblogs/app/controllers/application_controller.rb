ecd95ae44f6bd765aafcad20d9986bbe826593f8
Tiejun Li <tiejli@yahoo.com>
2016-07-16 23:31:53 -0700

    change help_method to herper_method.

--- railsblogs/app/controllers/application_controller.rb
+++ railsblogs/app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
-  help_method :current_user
+  helper_method :current_user
 
   private
 
