2f1380c71f31471dc3df6c2b4cd21acbb9ff9811
tieli <tiejli@yahoo.com>
2016-08-07 16:55:04 -0700

    add routes for password_resets.

--- blogs/config/routes.rb
+++ blogs/config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   get 'logout', to: 'sessions#destroy', as: 'logout'
 
   resources :sessions
-
+  resources :password_resets
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
