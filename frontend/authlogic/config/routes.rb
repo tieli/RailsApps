b60eda5848b4f9bbf57d6bd58e5995da095ae78c
tieli <tiejli@yahoo.com>
2016-11-30 01:46:03 +0000

    fixing tests for authlogic.

--- frontend/authlogic/config/routes.rb
+++ frontend/authlogic/config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
+  root to: 'welcome#home'
+  get 'welcome/home'
+
+  resources :users
   resources :users
 
   #resources :users, only: [:new, :create]
   resources :user_sessions, only: [:create, :destroy]
 
-  delete '/sign_out', to: 'user_sessions#destroy', as: :sign_out
-  get '/sign_in', to: 'user_sessions#new', as: :sign_in
+  delete 'logout', to: 'user_sessions#destroy', as: :logout
+  get 'login', to: 'user_sessions#new', as: :login
+  get 'signup', to: 'users#new', as: 'signup'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
